ace485b1dd13377d55191d416a65f4cb089b9bcb
dowoo kim <dowoo12345@naver.com>
2023-07-10 22:46:24 +0900

    fix: s3_utils.py

--- backend/image/s3_utils.py
+++ backend/image/s3_utils.py
@@ -1,8 +1,10 @@
 import boto3
 
-def upload_image_to_s3(img_file, t4y-bucket):
+import boto3
+
+def upload_image_to_s3(img_file, bucket_name):
     s3 = boto3.client('s3')
     key = img_file.name
-    s3.upload_fileobj(img_file, t4y-bucket, key)
-    img_url = f'https://{t4y-bucket}.s3.amazonaws.com/{key}'
+    s3.upload_fileobj(img_file, bucket_name, key)
+    img_url = f'https://{bucket_name}.s3.amazonaws.com/{key}'
     return img_url
