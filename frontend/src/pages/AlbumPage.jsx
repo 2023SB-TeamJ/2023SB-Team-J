e8137bcdd9ade977651d33620bc76aec6e22225c
nagosu <nozick1021@gmail.com>
2023-07-14 04:47:33 +0900

    feat : 사진 추가하기 버튼 추가

--- frontend/src/pages/AlbumPage.jsx
+++ frontend/src/pages/AlbumPage.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Masonry from 'react-masonry-css';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 // import UploadBtn from '../components/UploadBtn';
 import Header from '../components/HeaderAlbum';
 import AlbumDetailModal from '../components/AlbumDetailModal';
+import PlusBtn from '../assets/images/plusBtn.png';
 
 function AlbumPage() {
+  const navigate = useNavigate();
   const images = [
     'https://s3-alpha-sig.figma.com/img/bc16/dca6/87bcc750a17c8606010729684a71e24a?Expires=1690156800&Signature=Zg9jI3wu6OMplp1Gv6pxWzBA36c4U8lXHOtJw~hMjDgkkNiGu11UFDekP-zsQ70zY5eGbLzt00pRkS1IRu2rU-rKlhDz63oK6aIGMSThShqAI8zaKm3c6TzNOb2EtseD8AQBIVqkXbslw6xRrBkuBoV0GHbqavmUrBQhGnZRcoEtl~T9Y7lFbQfqedx78G7PY82qZ8ZSV3nIJrI4ZC4fPs~Ha0iPwPxvbn0XQ0W3qL7gcDidY4rGIYy85abedhI6JkF5vHjeYblNXZgHdzDRUmiVdW-8ORkVkg~zvGorzdTdbsjbyQph02VMLHbnNi4LLpWxvR10OB84TaJ-Nngawg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     'https://s3-alpha-sig.figma.com/img/33b6/231f/4ef9af44b444ede0015278dbafbea50a?Expires=1690156800&Signature=LgDA0fZ99FMtk3ZHwtTT~rmd-BBTGyMF1rLwQ1dl0r2pegJgIcPCbld1be420GLNQ1vn80sOAjlkHvIMX3gbgqsLKoeIvlLGsKxz4mRpJjo~8HUqmMVlrZTWA59cxnQIH4o7uNGugvfUQlM6aBmYXD1yCiva5uaj~ftc~SitoVOw6m4O41NIvvt2PSjhQZ0WXlAHZRgx5i7MDZ165RC~DpeDx0dDRAoNqpniwytoC~X7U2Krpg~T089gjIzCG7uSITwAFkW598c4ln4RXwZcWUH2ArJhdyhJKE7QbNtXzVulDLCd60beYdhP071g54i85-uVOizFlbMTTQ-H2vah8g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
@@ -36,6 +40,11 @@ function AlbumPage() {
       <Container>
         <Header />
         <MainWrap>
+          <AddBtn
+            onClick={() => navigate('/choose')}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ borderRadius: '50%' }}
+          />
           <MyMasonryGrid
             breakpointCols={breakpointColumnObj}
             className="my-masonry-grid"
@@ -72,6 +81,7 @@ const Container = styled.div`
 `;
 
 const MainWrap = styled.div`
+  position: relative;
   max-width: 1440px;
   width: 76vw;
   height: 100%;
@@ -81,8 +91,23 @@ const MainWrap = styled.div`
   background-color: white;
 `;
 
+const AddBtn = styled(motion.div)`
+  position: absolute;
+  top: 10vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 60px; // width 추가
+  height: 60px; // height 추가
+  background: url(${PlusBtn}) no-repeat center/cover;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 const MyMasonryGrid = styled(Masonry)`
   display: flex;
+  margin-top: 30vh;
   margin-left: -30px; /* 컬럼 간격을 조절하기 위해 음수 마진을 적용 */
   width: auto;
 
@@ -106,10 +131,3 @@ const MyMasonryGridColumn = styled.div`
 const ImageWithShadow = styled.img`
   box-shadow: 10px 10px 6px 0px rgba(0, 0, 0, 0.25);
 `;
-
-// const CenteredButton = styled.div`
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-// `;
