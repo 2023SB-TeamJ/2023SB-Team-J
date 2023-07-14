758d62a9847a701abc43be684101d7d20f864c7e
nagosu <nozick1021@gmail.com>
2023-07-14 16:29:18 +0900

    style : 메인페이지 구현 완료

--- frontend/src/components/AlbumDetailModal.jsx
+++ frontend/src/components/AlbumDetailModal.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
 import image from '../assets/images/photo1.png';
-// import star from '../assets/images/star.png';
-// import trash from '../assets/images/trash.png';
-// import save from '../assets/images/save.png';
 
 function AlbumDetailModal() {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,12 +24,7 @@ function AlbumDetailModal() {
       {/* 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현 */}
       {isOpen ? (
         <ModalBackdrop onClick={openModalHandler}>
-          <ModalView
-            onClick={(e) => e.stopPropagation()}
-            initial={{ y: -1000 }}
-            animate={{ y: 0 }}
-            transition={{ duration: 0.5 }}
-          >
+          <ModalView onClick={(e) => e.stopPropagation()}>
             <Photo />
             <Date>2023.07.12</Date>
             <ButtonWrap>
@@ -275,7 +266,7 @@ const ModalBackdrop = styled.div`
 //   background-color: #3f3d3f;
 // `;
 
-const ModalView = styled(motion.div)`
+const ModalView = styled.div`
   position: relative;
   display: flex;
   align-items: center;
