bc5cc2054652c823ca8399b3137387ab6d5b2efb
alpakaka0o0 <alpakaka000808@gmail.com>
2023-07-18 00:23:15 +0900

    fix: try 문 제거

--- backend/album/views.py
+++ backend/album/views.py
@@ -11,46 +11,41 @@ from .serializers import AlbumDetailSerializer
 
 class AlbumDetailView(APIView): #album/detail
     def put(self, request, format=None): #결과 이미지 삭제
+
+        raw_data = request.body.decode('utf-8')
+
         try:
-            raw_data = request.body.decode('utf-8')
+            data = json.loads(raw_data)
+            user_id = data.get('user_id')
+            result_image_id = data.get('result_image_id')
 
-            try:
-                data = json.loads(raw_data)
-                user_id = data.get('user_id')
-                result_image_id = data.get('result_image_id')
-
-                if user_id is None or result_image_id is None:  # request 형식에 맞지 않는 경우
-                    return Response(status=status.HTTP_400_BAD_REQUEST)
-
-                image_collage = Image_collage.objects.get(id=result_image_id, user_id=user_id, deleted_at__isnull=True)
-
-                image_collage.deleted_at = timezone.localtime(timezone.now())
-                image_collage.save()
-
-                return Response(status=status.HTTP_200_OK)
-            except:
-                # 해당 객체를 찾지 못한 경우 HTTP_400
+            if user_id is None or result_image_id is None:  # request 형식에 맞지 않는 경우
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        except Exception as e:
-            return JsonResponse({"error message": str(e)}, status=500)
+            image_collage = Image_collage.objects.get(id=result_image_id, user_id=user_id, deleted_at__isnull=True)
+
+            image_collage.deleted_at = timezone.localtime(timezone.now())
+            image_collage.save()
+
+            return Response(status=status.HTTP_200_OK)
+        except:
+            # 해당 객체를 찾지 못한 경우 HTTP_400
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
     def get(self, request, format=None): #앨범 상세 조회
+
+        result_image_id = request.data.get('result_image_id')
         try:
-            result_image_id = request.data.get('result_image_id')
+            image_collage = Image_collage.objects.get(id=result_image_id, deleted_at__isnull=True)
+        except:
+            # 찾지 못한 경우
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = AlbumDetailSerializer(image_collage)
 
-            try:
-                image_collage = Image_collage.objects.get(id=result_image_id, deleted_at__isnull=True)
-            except:
-                # 찾지 못한 경우
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            serializer = AlbumDetailSerializer(image_collage)
-
-            #form data로 어떻게 보내나용..?
-            response = {
-                "result_image": serializer.data.get('result_url'),
-                "create_date": serializer.data.get('created_at')
-            }
-            return Response(response, status=status.HTTP_200_OK)
-        except Exception as e:
-            return JsonResponse({"error message": str(e)}, status=500)
+        #form data로 어떻게 보내나용..?
+        response = {
+            "result_image": serializer.data.get('result_url'),
+            "create_date": serializer.data.get('created_at')
+        }
+        return Response(response, status=status.HTTP_200_OK)
--- backend/image/views.py
+++ backend/image/views.py
@@ -29,68 +29,62 @@ class UploadImageView(APIView):
     # permission_classes = [IsAuthenticated] #권한 있는 사람, 로그인 한 사람만 접근 가능
 
     def post(self, request):
-        try:
-            serializer = UploadedImageSerializer(data=request.data)
-            if serializer.is_valid():
-                # 이미지 저장
-                img_files = request.FILES.getlist('img_files')
-                img_urls = []
+        serializer = UploadedImageSerializer(data=request.data)
+        if serializer.is_valid():
+            # 이미지 저장
+            img_files = request.FILES.getlist('img_files')
+            img_urls = []
 
-                for img_file in img_files:
-                    # S3 버킷에 이미지 업로드
-                    with Image.open(img_file) as im:
-                        im_jpeg = BytesIO()
-                        im.save(im_jpeg, 'JPEG')
-                        im_jpeg.seek(0)
-                    key = request.data.get("user_id") + str(datetime.now()).replace('.', '') + "." + "jpeg"
-                    img_url = upload_image_to_s3(im_jpeg, key, ExtraArgs={'ContentType': "image/jpeg"})
-                    img_urls.append(img_url)
+            for img_file in img_files:
+                # S3 버킷에 이미지 업로드
+                with Image.open(img_file) as im:
+                    im_jpeg = BytesIO()
+                    im.save(im_jpeg, 'JPEG')
+                    im_jpeg.seek(0)
+                key = request.data.get("user_id") + str(datetime.now()).replace('.', '') + "." + "jpeg"
+                img_url = upload_image_to_s3(im_jpeg, key, ExtraArgs={'ContentType': "image/jpeg"})
+                img_urls.append(img_url)
 
-                # 이미지 URL MySQL에 저장
-                data = {
-                    'user_id': serializer.validated_data['user_id'],
-                    'url_1': img_urls[0] if len(img_urls) > 0 else '',
-                    'url_2': img_urls[1] if len(img_urls) > 1 else '',
-                    'url_3': img_urls[2] if len(img_urls) > 2 else '',
-                    'url_4': img_urls[3] if len(img_urls) > 3 else '',
-                }
-                uploaded_image = Image_origin.objects.create(**data)
-                serializer = UploadedImageSerializer(uploaded_image)
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return JsonResponse({"error message": str(e)}, status=500)
+            # 이미지 URL MySQL에 저장
+            data = {
+                'user_id': serializer.validated_data['user_id'],
+                'url_1': img_urls[0] if len(img_urls) > 0 else '',
+                'url_2': img_urls[1] if len(img_urls) > 1 else '',
+                'url_3': img_urls[2] if len(img_urls) > 2 else '',
+                'url_4': img_urls[3] if len(img_urls) > 3 else '',
+            }
+            uploaded_image = Image_origin.objects.create(**data)
+            serializer = UploadedImageSerializer(uploaded_image)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, format=None):
+        raw_data = request.body.decode('utf-8')
+
         try:
-            raw_data = request.body.decode('utf-8')
+            data = json.loads(raw_data)
+            user_id = data.get('user_id')
+            source = data.get('source')
 
-            try:
-                data = json.loads(raw_data)
-                user_id = data.get('user_id')
-                source = data.get('source')
+            if user_id is None or source is None:  # request 형식에 맞지 않는 경우
+                return Response({"error message": "request 형식이 맞지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-                if user_id is None or source is None:  # request 형식에 맞지 않는 경우
-                    return Response({"error message": "request 형식이 맞지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+            image_origin = Image_origin.objects.get(id=source, user_id=user_id, deleted_at__isnull=True)
 
-                image_origin = Image_origin.objects.get(id=source, user_id=user_id, deleted_at__isnull=True)
+        except:
+            # 찾지 못한 경우 HTTP_400
+            return Response({"error message": "해당되는 데이터가 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-            except:
-                # 찾지 못한 경우 HTTP_400
-                return Response({"error message": "해당되는 데이터가 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = UploadedImageSerializer(image_origin)
 
-            serializer = UploadedImageSerializer(image_origin)
-
-            picture = {
-                'url_1': serializer.data.get('url_1'),
-                'url_2': serializer.data.get('url_2'),
-                'url_3': serializer.data.get('url_3'),
-                'url_4': serializer.data.get('url_4'),
-            }
-            return Response(picture, status=status.HTTP_200_OK)
-        except Exception as e:
-            return JsonResponse({"error message": str(e)}, status=500)
+        picture = {
+            'url_1': serializer.data.get('url_1'),
+            'url_2': serializer.data.get('url_2'),
+            'url_3': serializer.data.get('url_3'),
+            'url_4': serializer.data.get('url_4'),
+        }
+        return Response(picture, status=status.HTTP_200_OK)
 
 
 class AiExecute(APIView):
