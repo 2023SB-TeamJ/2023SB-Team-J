a544eaa2c3b8d710580adc140133ace9e9df7e6c
nagosu <nozick1021@gmail.com>
2023-07-17 21:14:34 +0900

    chore : 로그인/회원가입 이메일로 수정

--- frontend/src/components/PageShiftBtn.jsx
+++ frontend/src/components/PageShiftBtn.jsx
@@ -1,15 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-function PageShiftBtn({ path }) {
+function PageShiftBtn({ path, onClick }) {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    } else {
+      navigate(path);
+    }
+  };
+
   return (
-    <PageContainer
-      onClick={() => {
-        navigate(path);
-      }}
-    >
+    <PageContainer onClick={handleClick}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 77 77" fill="none">
         <g filter="url(#filter0_d_606_413)">
           <circle cx="38.5" cy="34.5" r="33" stroke="#151515" strokeWidth="3" />
--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from 'styled-components';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import UploadImage from '../components/UploadImage';
+import PageShiftBtn from '../components/PageShiftBtn';
 
 function TestPage() {
+  const [files, setFiles] = useState([]);
+  const navigate = useNavigate();
+
+  const onImageUpload = (file) => {
+    setFiles((prevFiles) => [...prevFiles, file]);
+  };
+
+  const uploadAllImages = () => {
+    const formData = new FormData();
+    const userId = 1;
+    formData.append('user_id', userId);
+    files.forEach((file) => {
+      formData.append(`img_files`, file);
+    });
+
+    axios
+      .post('http://localhost:8000/api/v1/frame/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        navigate('/ConvertAIPage');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log([...formData.entries()]);
+  };
+
   return (
     <div>
       <Container>
         <MainWrap>
           <div>테스트 페이지 입니다</div>
-          <UploadImage />
-          <UploadImage />
-          <UploadImage />
-          <UploadImage />
+          <UploadImage onImageUpload={onImageUpload} />
+          <UploadImage onImageUpload={onImageUpload} />
+          <UploadImage onImageUpload={onImageUpload} />
+          <UploadImage onImageUpload={onImageUpload} />
+          <PageShiftBtn onClick={uploadAllImages} />
         </MainWrap>
       </Container>
     </div>
