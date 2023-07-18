68f5a2026f6f41c84ef42f73b9ba75e1168515da
dowoo kim <dowoo12345@naver.com>
2023-07-18 14:53:57 +0900

    album modle fix

--- backend/album/models.py
+++ backend/album/models.py
@@ -17,7 +17,6 @@ class Image_chosen(models.Model):
 
 class Image_collage(models.Model):
     user_id = models.ForeignKey('common.User', on_delete=models.RESTRICT)
-    #img_chosen_id = models.ForeignKey('Image_chosen', on_delete=models.RESTRICT)
     img_origin_id = models.ForeignKey('image.Image_origin', on_delete=models.RESTRICT)
     result_url = models.URLField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)  # 생성일 필드 추가
