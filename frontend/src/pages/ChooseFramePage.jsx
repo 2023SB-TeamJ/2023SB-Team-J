ead7ada41b0a6a5c34c1ee4d618a78b2dd69439c
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-07-18 12:51:09 +0900

    feat : 프레임 선택/이미지 업로드 페이지 구현

--- frontend/src/pages/ChooseFramePage.jsx
+++ frontend/src/pages/ChooseFramePage.jsx
@@ -1,10 +1,25 @@
-import { React } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import Header from '../components/Header';
 import Title from '../components/Title';
-import PageShiftBtn from '../components/PageShiftBtn';
+import oneFourImage from '../assets/images/1x4sampleImage.png';
+import twoTwoImage from '../assets/images/2x2sampleImage.jpg';
 
 function ChooseFramePage() {
+  const [frameType] = useState('');
+  const navigate = useNavigate();
+
+  const handleImageClick1 = () => {
+    // upload 경로로 이동하면서 state 값을 함께 전달한다.
+    navigate('/upload', { state: { frameType: '1X4' } });
+  };
+
+  const handleImageClick2 = () => {
+    navigate('/upload', { state: { frameType: '2X2' } });
+  };
   return (
     <div>
       <Container>
@@ -17,9 +32,33 @@ function ChooseFramePage() {
             프로그레스 바/프로그레스 바/프로그레스 바/프로그레스 바/프로그레스
             바/프로그레스 바/프로그레스 바
           </ProgressBar>
-          <PageShiftWrap>
-            <PageShiftBtn path="/upload" />
-          </PageShiftWrap>
+          <FrameWrap>
+            <ImgWrap
+              // 1X4 배열 형태의 컴포넌트로 이루어진 페이지로 이동한다.
+              onClick={() => {
+                handleImageClick1();
+              }}
+            >
+              <motion.img
+                src={oneFourImage}
+                alt="1x4이미지"
+                whileHover={{ scale: 1.1 }}
+              />
+            </ImgWrap>
+
+            <ImgWrap
+              // 2X2 배열 형태의 컴포넌트로 이루어진 페이지로 이동한다.
+              onClick={() => {
+                handleImageClick2();
+              }}
+            >
+              <motion.img
+                src={twoTwoImage}
+                alt="2x2이미지"
+                whileHover={{ scale: 1.1 }}
+              />
+            </ImgWrap>
+          </FrameWrap>
         </MainWrap>
       </Container>
     </div>
@@ -55,7 +94,33 @@ const ProgressBar = styled.div`
   margin: 3rem;
 `;
 
-const PageShiftWrap = styled.div`
+// const PageShiftWrap = styled.div`
+//   display: flex;
+//   justify-content: center;
+// `;
+
+const FrameWrap = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
+
+const ImgWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 60%;
+    display: block;
+    margin-bottom: 20px;
+    margin-left: 60px;
+  }
+`;
+
+// const Img1X4 = styled(motion.img)`
+// whileHover={{ scale: 1.1 }}
+// `;
+
+// const Img2X2 = styled(motion.img)`
+//   whileHover={{ scale: 1.1 }}
+//
--- frontend/src/pages/UploadImagePage.jsx
+++ frontend/src/pages/UploadImagePage.jsx
@@ -1,10 +1,20 @@
-import { React } from 'react';
+/* eslint-disable consistent-return */
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import Title from '../components/Title';
 import PageShiftBtn from '../components/PageShiftBtn';
+import UploadImage from '../components/UploadImage';
 
 function UploadImagePage() {
+  // locainon 객체를 사용하기 위해 useLocation() 훅을 사용해야 한다.
+  const location = useLocation();
+  // loaction 객체 속성인 state 값(이전 페이지에 전달된 상태값)을 가지고 와서 frameType에 저장한다.
+  const { frameType } = location.state;
+
+  // UploadImage 컴포넌트 4개로 이루어진 배열을 생성한다.
+  const uploadImageComponents = Array(4).fill(<UploadImage />);
   return (
     <div>
       <Container>
@@ -20,6 +30,9 @@ function UploadImagePage() {
           <PageShiftWrap>
             <PageShiftBtn path="/convert" />
           </PageShiftWrap>
+          <ImageWrapper frameType={frameType}>
+            {uploadImageComponents}
+          </ImageWrapper>
         </MainWrap>
       </Container>
     </div>
@@ -57,5 +70,30 @@ const ProgressBar = styled.div`
 
 const PageShiftWrap = styled.div`
   display: flex;
+  justify-content: flex-end;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
   justify-content: center;
+  align-items: center;
+
+  ${({ frameType }) => {
+    // frameType prop을 사용하여 스타일링 변경
+    if (frameType === '1X4') {
+      return `
+        flex-direction: column;
+        gap: 20px;
+        height: 2rem;
+      `;
+    }
+    if (frameType === '2X2') {
+      return `
+        display: grid;
+        grid-template-rows: repeat(2, 200px);
+        grid-template-columns: repeat(2, 0.2fr);
+      `;
+    }
+    // frameType에 따라 다른 스타일링을 적용하거나, 기본 스타일링을 반환할 수 있습니다.
+  }}
 `;
