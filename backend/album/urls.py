e727d9875fc502e00b6da7d9f62b387904366c69
aristo <dkfud2121@naver.com>
2023-07-23 06:35:02 +0900

    fix : 오타 수정

--- backend/album/urls.py
+++ backend/album/urls.py
@@ -6,5 +6,5 @@ app_name = "album"
 urlpatterns = [
     # 루트 URL 패턴
     path('detail/', AlbumDetailView.as_view(), name='detail_root'),
-bumView.as_view(), name='album')
+    path('', AlbumView.as_view(), name='album')
 ]
