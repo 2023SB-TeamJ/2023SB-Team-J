a0a85b7284f217740b3cd1e62883e98bca9c74cb
alpakaka0o0 <alpakaka000808@gmail.com>
2023-07-27 01:26:37 +0900

    error: mysqld_exporter db 연결 안됨

--- backend/image/models.py
+++ backend/image/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django_prometheus.models import ExportModelOperationsMixin
 
 
 # from backend.common.models import User
 # from ..common.models import User
 
-class Image_upload(models.Model):
+class Image_upload(ExportModelOperationsMixin('Image_upload'), models.Model):
     user_id = models.ForeignKey('common.User', on_delete=models.RESTRICT)
     url = models.URLField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)  # 생성일 필드 추가
@@ -12,7 +13,7 @@ class Image_upload(models.Model):
     state = models.BooleanField(default=1, null=False)
     is_selected = models.BooleanField(default=0, null=False)
 
-class Ai_model(models.Model):
+class Ai_model(ExportModelOperationsMixin('Ai_model'), models.Model):
     image_upload_id = models.ForeignKey(Image_upload, on_delete=models.RESTRICT)
     model_name = models.CharField(max_length=20)
     model_result_url = models.URLField(null=False)
