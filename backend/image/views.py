8222d6a73c0366d2206aa0cde42179ba9e4395ca
aristo <dkfud2121@naver.com>
2023-07-28 16:03:55 +0900

    fix: 권한 인증 후 [post]api/v1/frame 오류 해결

--- backend/image/views.py
+++ backend/image/views.py
@@ -1,31 +1,39 @@
+import pickle
 import time
 
+import json
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
-from album.serializers import ResultImageSerializer
 
 from .AiTask import *
 from .s3_utils import *
 from .models import *
+from rest_framework.permissions import AllowAny
 
 from album.serializers import *
+
 from common.utils import user_token_to_data
 
+
 class UploadImageView(APIView):
-    permission_classes = [IsAuthenticated]
-    
+    permission_classes = [AllowAny]
+
     def post(self, request):
+        print(">>> 1. in api v1 frame >>>")
+        image = request.data.get("image")
         authorization_header = request.META.get('HTTP_AUTHORIZATION')
         if authorization_header and authorization_header.startswith('Bearer '):
             token = authorization_header.split(' ')[1]
-            user_id=user_token_to_data(token)
+            user_id = user_token_to_data(token)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        image = request.data.get("image")
+        if not user_id:  # user_id가 제공되지 않았을 경우에 대한 처리
+            return Response({"error": "User ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
         with Image.open(image) as im:
             im = im.convert("RGB")
             im_jpeg = BytesIO()
@@ -37,7 +45,9 @@ class UploadImageView(APIView):
             "user_id": user_id,
             "url": img_url
         }
+        print(">>> 2. data >>> ", data)
         serializer = UploadedImageSerializer(data=data)
+        print(">>> 3. after serializer >>>", serializer)
         if serializer.is_valid():
             serializer.save()
             response = {
@@ -48,8 +58,35 @@ class UploadImageView(APIView):
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#     def get(self, request, format=None):
+#         raw_data = request.body.decode('utf-8')
+#         try:
+#             data = json.loads(raw_data)
+#             user_id = data.get('user_id')
+#             source = data.get('source')
+
+#             if user_id is None or source is None:  # request 형식에 맞지 않는 경우
+#                 return Response(status=status.HTTP_400_BAD_REQUEST)
+
+#             image_origin = Image_upload.objects.get(id=source, user_id=user_id, deleted_at__isnull=True)
+
+#         except:
+#             # 찾지 못한 경우 HTTP_400
+#             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+#         serializer = UploadedImageSerializer(image_origin)
+
+#         picture = {
+#             'url_1': serializer.data.get('url_1'),
+#             'url_2': serializer.data.get('url_2'),
+#             'url_3': serializer.data.get('url_3'),
+#             'url_4': serializer.data.get('url_4'),
+#         }
+#         return Response(picture, status=status.HTTP_200_OK)
+
 class AiExecute(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def post(self, request):
         url = request.data.get("image")
@@ -106,15 +143,9 @@ class AiExecute(APIView):
 
 
 class ResultImageView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     def post(self, request):
-        authorization_header = request.META.get('HTTP_AUTHORIZATION')
-        if authorization_header and authorization_header.startswith('Bearer '):
-            token = authorization_header.split(' ')[1]
-            user_id=user_token_to_data(token)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
+        user_id = request.data.get("user_id")
         result_image = request.data.get("result_image")
         im = Image.open(result_image)
         im = im.convert("RGB")
@@ -123,6 +154,7 @@ class ResultImageView(APIView):
         im_jpeg.seek(0)
         key = "Result_image/" + generate_unique_filename(im_jpeg.getvalue()) + ".jpeg"
         img_url = upload_image_to_s3(im_jpeg, key, ExtraArgs={'ContentType': "image/jpeg"})
+
         data = {
             "user_id": user_id,
             "result_url": img_url
