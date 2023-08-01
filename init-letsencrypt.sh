47d51454d44912654ba494658a1168cc5bf462c3
dowoo kim <dowoo12345@naver.com>
2023-08-02 01:54:18 +0900

    fix docker-compose

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(thiss4u.site)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certbot"
 email="dkfud2121@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
@@ -77,4 +77,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose -f "docker-compose.prod.yml" exec nginx nginx -s reload
+docker-compose exec nginx nginx -s reload
