c3227b93e11d718800e31063ea74f13bf1db0a29
kubit2 <datakubit2@gmail.com>
2023-08-02 01:46:51 +0900

    feat:https

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,10 +6,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 compose_file="docker-compose.prod.yml"
-domains=(gdiary.shop)
+domains=(thiss4u.site)
 rsa_key_size=4096
 data_path="./certbot"
-email=" " # Adding a valid address is strongly recommended
+email="dkfud2121@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
