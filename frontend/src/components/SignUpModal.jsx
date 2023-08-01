3dfe903eb025b07bea1fe9733429c22c872ba623
dowoo kim <dowoo12345@naver.com>
2023-08-01 19:56:53 +0900

    fix signupModal

--- frontend/src/components/SignUpModal.jsx
+++ frontend/src/components/SignUpModal.jsx
@@ -53,7 +53,7 @@ function SignUpModal({ isOpen, onClose }) {
         password,
       };
       console.log(data);
-      const response = await axios.post(`${apiUrl}login/`, data);
+      const response = await axios.post(`${apiUrl}signup/`, data);
       console.log(response.status); // 실제 반환되는 상태 코드 확인
       // 응답 확인
       if (response.status === 201) {
