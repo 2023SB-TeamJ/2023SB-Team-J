0ed68e97a7119e6fd8467986a67ea919cccc391f
dowoo kim <dowoo12345@naver.com>
2023-08-01 21:21:41 +0900

    chage model

--- backend/image/AiModels.py
+++ backend/image/AiModels.py
@@ -23,8 +23,8 @@ class Models:
             # 알파 채널 제거하고 RGB 형식으로 변환
             image = image.convert("RGB")
 
-        image = image.resize((256, 256))
-        result = Models.face2paint(Models.model1, image, size=256).resize((240, 160))
+        image = image.resize((518, 518))
+        result = Models.face2paint(Models.model1, image, size=518)
         with BytesIO() as file:
             result.save(file, format='JPEG')
             file.seek(0)
@@ -39,8 +39,8 @@ class Models:
             # 알파 채널 제거하고 RGB 형식으로 변환
             image = image.convert("RGB")
 
-        image = image.resize((256, 256))
-        result = Models.face2paint(Models.model2, image, size=256).resize((240, 160))
+        image = image.resize((518, 518))
+        result = Models.face2paint(Models.model2, image, size=518)
         with BytesIO() as file:
             result.save(file, format='JPEG')
             file.seek(0)
@@ -55,8 +55,8 @@ class Models:
         if channels == "RGBA":
             # 알파 채널 제거하고 RGB 형식으로 변환
             image = image.convert("RGB")
-        image = image.resize((256, 256))
-        result = Models.face2paint(Models.model3, image, size=256).resize((240, 160))
+        image = image.resize((518, 518))
+        result = Models.face2paint(Models.model3, image, size=518)
         with BytesIO() as file:
             result.save(file, format='JPEG')
             file.seek(0)
