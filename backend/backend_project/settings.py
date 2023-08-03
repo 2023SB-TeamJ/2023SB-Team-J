bb8def6ec710d9bbf666e8ea54379e17e7904f0f
dowoo kim <dowoo12345@naver.com>
2023-08-03 21:18:37 +0900

    fix limit file size

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -330,3 +330,8 @@ SIMPLE_JWT = {
 }
 BROKER_HEARTBEAT=60 # 60 seconds for the heartbeat signal
 BROKER_HEARTBEAT_CHECKRATE=2.0 # Check for a heartbeat every 2 seconds
+# settings.py
+
+# 사진 등의 파일 업로드 용량 제한 설정 (10MB로 설정)
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
+MEDIA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
